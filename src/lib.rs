45b5558fcc45f6dd64c955f5969df266bd64f674
Rudranil Sarkar <rudronil34@gmail.com>
2024-05-11 14:06:55 +0530

    Added encoding support

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,60 @@
-use std::collections::HashMap;
-use std::fmt;
-
 pub mod bencode {
+    use std::collections::HashMap;
+    use std::fmt;
 
-    use super::*;
+    pub enum BencodeError {
+        ParseStringError(String, String),
+        ParseIntgerError(String, String),
+        ParseListError(String, String),
+        ParseDictError(String, String),
+        InvalidElementError(String, String),
+    }
+
+    impl fmt::Display for BencodeError {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            match self {
+                Self::ParseStringError(x, y) => {
+                    write!(f, "Failed to Parse Bencode String: {} in {}", x, y)
+                }
+                Self::ParseIntgerError(x, y) => {
+                    write!(f, "Failed to Parse Bencode Integer: {} in {}", x, y)
+                }
+                Self::ParseListError(x, y) => {
+                    write!(f, "Failed to Parse Bencode List: {} in {}", x, y)
+                }
+                Self::ParseDictError(x, y) => {
+                    write!(f, "Failed to Parse Bencode Dict: {} in {}", x, y)
+                }
+                Self::InvalidElementError(x, y) => {
+                    write!(f, "Invalid Element: {} in {}", x, y)
+                }
+            }
+        }
+    }
+
+    impl fmt::Debug for BencodeError {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            match self {
+                Self::ParseStringError(x, y) => {
+                    write!(f, "Failed to Parse Bencode String: {} in {}", x, y)
+                }
+                Self::ParseIntgerError(x, y) => {
+                    write!(f, "Failed to Parse Bencode Integer: {} in {}", x, y)
+                }
+                Self::ParseListError(x, y) => {
+                    write!(f, "Failed to Parse Bencode List: {} in {}", x, y)
+                }
+                Self::ParseDictError(x, y) => {
+                    write!(f, "Failed to Parse Bencode Dict: {} in {}", x, y)
+                }
+                Self::InvalidElementError(x, y) => {
+                    write!(f, "Invalid Element: {} in {}", x, y)
+                }
+            }
+        }
+    }
+    // Start of Decoding Logic
+    // -------------------------------------------------------------------------------------------------
     #[derive(Debug, Clone)]
     pub enum StringorByteArray {
         StringAble(String),
@@ -131,58 +182,6 @@ pub mod bencode {
                     print_str.pop();
                     print_str.push(']');
                     write!(f, "{}", print_str)
-                }
-            }
-        }
-    }
-
-    pub enum BencodeError {
-        ParseStringError(String, String),
-        ParseIntgerError(String, String),
-        ParseListError(String, String),
-        ParseDictError(String, String),
-        InvalidElementError(String, String),
-    }
-
-    impl fmt::Display for BencodeError {
-        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-            match self {
-                Self::ParseStringError(x, y) => {
-                    write!(f, "Failed to Parse Bencode String: {} in {}", x, y)
-                }
-                Self::ParseIntgerError(x, y) => {
-                    write!(f, "Failed to Parse Bencode Integer: {} in {}", x, y)
-                }
-                Self::ParseListError(x, y) => {
-                    write!(f, "Failed to Parse Bencode List: {} in {}", x, y)
-                }
-                Self::ParseDictError(x, y) => {
-                    write!(f, "Failed to Parse Bencode Dict: {} in {}", x, y)
-                }
-                Self::InvalidElementError(x, y) => {
-                    write!(f, "Invalid Element: {} in {}", x, y)
-                }
-            }
-        }
-    }
-
-    impl fmt::Debug for BencodeError {
-        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            match self {
-                Self::ParseStringError(x, y) => {
-                    write!(f, "Failed to Parse Bencode String: {} in {}", x, y)
-                }
-                Self::ParseIntgerError(x, y) => {
-                    write!(f, "Failed to Parse Bencode Integer: {} in {}", x, y)
-                }
-                Self::ParseListError(x, y) => {
-                    write!(f, "Failed to Parse Bencode List: {} in {}", x, y)
-                }
-                Self::ParseDictError(x, y) => {
-                    write!(f, "Failed to Parse Bencode Dict: {} in {}", x, y)
-                }
-                Self::InvalidElementError(x, y) => {
-                    write!(f, "Invalid Element: {} in {}", x, y)
                 }
             }
         }
@@ -332,22 +331,118 @@ pub mod bencode {
     pub fn decode_bencode_element(bencode_str: Vec<u8>) -> Result<BencodeElement> {
         Ok(decoder_internal(&bencode_str[..])?.1)
     }
+
+    // -------------------------------------------------------------------------------------------------
+    // End of Decoding Logic
+
+    // Start of Encoding Logic
+    // -------------------------------------------------------------------------------------------------
+
+    pub enum BencodeEncodeble {
+        Number(i64),
+        String(String),
+        List(Vec<BencodeEncodeble>),
+        Dict(HashMap<String, BencodeEncodeble>),
+    }
+
+    impl Into<BencodeEncodeble> for i64 {
+        fn into(self) -> BencodeEncodeble {
+            BencodeEncodeble::Number(self)
+        }
+    }
+
+    impl Into<BencodeEncodeble> for &str {
+        fn into(self) -> BencodeEncodeble {
+            BencodeEncodeble::String(self.to_string())
+        }
+    }
+
+    impl Into<BencodeEncodeble> for String {
+        fn into(self) -> BencodeEncodeble {
+            BencodeEncodeble::String(self)
+        }
+    }
+
+    impl Into<BencodeEncodeble> for Vec<BencodeEncodeble> {
+        fn into(self) -> BencodeEncodeble {
+            BencodeEncodeble::List(self)
+        }
+    }
+
+    impl Into<BencodeEncodeble> for HashMap<String, BencodeEncodeble> {
+        fn into(self) -> BencodeEncodeble {
+            BencodeEncodeble::Dict(self)
+        }
+    }
+
+    fn encode_integer_bencode(i: i64) -> String {
+        format!("i{}e", i)
+    }
+
+    fn encode_string_bencode(s: &str) -> String {
+        format!("{}:{}", s.len(), s)
+    }
+
+    pub fn encode_bencode_value(value: &BencodeEncodeble) -> Result<String> {
+        match value {
+            BencodeEncodeble::Number(num) => Ok(encode_integer_bencode(*num)),
+            BencodeEncodeble::String(str) => Ok(encode_string_bencode(str)),
+            BencodeEncodeble::List(lst) => {
+                let mut res_str: String = String::from("l");
+                for i in lst {
+                    res_str.push_str(&encode_bencode_value(i)?)
+                }
+
+                res_str.push('e');
+
+                Ok(res_str)
+            }
+            BencodeEncodeble::Dict(dict) => {
+                let mut res_str = String::from("d");
+                for (key, value) in dict.iter() {
+                    res_str.push_str(encode_bencode_value(&key.clone().into())?.as_str());
+                    res_str.push_str(encode_bencode_value(value)?.as_str())
+                }
+                res_str.push('e');
+                Ok(res_str)
+            }
+        }
+    }
+
+    // -------------------------------------------------------------------------------------------------
+    // End of Encoding Logic
 }
 
 #[cfg(test)]
 mod test {
     use std::collections::HashMap;
 
-    use crate::bencode::{decode_bencode_element, BencodeElement};
+    use crate::bencode::BencodeEncodeble;
+
+    use super::*;
 
     #[test]
     fn test() {
-        let V: Vec<u8> = "d5:helloi5ee".as_bytes().to_vec();
-        let x: HashMap<String, BencodeElement> =
-            decode_bencode_element(V).unwrap().try_into().unwrap();
+        let lst: Vec<BencodeEncodeble> = vec![
+            1.into(),
+            2.into(),
+            3.into(),
+            vec![4.into(), 5.into()].into(),
+            "Hello".into(),
+            vec![5.into(), 6.into(), vec![7.into(), 8.into()].into()].into(),
+        ];
+
+        let mut dict: HashMap<String, BencodeEncodeble> = HashMap::new();
+
+        dict.insert("test".to_string(), lst.into());
+        let mut dict2: HashMap<String, BencodeEncodeble> = HashMap::new();
+        dict2.insert("hello".to_string(), 123.into());
+        dict.insert("test2".to_string(), dict2.into());
+
+        let str = bencode::encode_bencode_value(&dict.into()).unwrap();
         assert_eq!(
-            <BencodeElement as TryInto<i64>>::try_into(x.get("hello").unwrap().clone()).unwrap(),
-            5
-        );
+            str,
+            "d4:testli1ei2ei3eli4ei5ee5:Helloli5ei6eli7ei8eeee5:test2d5:helloi123eee"
+        )
     }
 }
